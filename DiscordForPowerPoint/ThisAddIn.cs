6b3e34171aba99029664db9587b3905a3bca08a8
Leondro Lio <admin@moustacheminer.com>
2018-07-26 19:27:11 +0100

    Trick Microsoft PowerPoint into doing RPC

--- DiscordForPowerPoint/ThisAddIn.cs
+++ DiscordForPowerPoint/ThisAddIn.cs
@@ -5,24 +5,54 @@ using System.Text;
 using System.Xml.Linq;
 using PowerPoint = Microsoft.Office.Interop.PowerPoint;
 using Office = Microsoft.Office.Core;
+using System.Diagnostics;
+using DiscordRPC;
+using DiscordRPC.Logging;
+using DiscordRPC.Message;
+using System.Threading;
 
 namespace DiscordForPowerPoint
 {
     public partial class ThisAddIn
     {
+        public DiscordRpcClient client;
+        private static int DiscordPipe = -1;
+        private static string ClientID = "470239659591598091";
+        private static LogLevel DiscordLogLevel = LogLevel.Info;
+
+        private static RichPresence presence = new RichPresence()
+        {
+            Details = "Untitled Presentation",
+            State = "Editing",
+            Assets = new Assets()
+            {
+                LargeImageKey = "welcome",
+                LargeImageText = "Not Editing",
+                SmallImageKey = "powerpoint"
+            }
+        };
+
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
-            this.Application.PresentationNewSlide +=
-    new PowerPoint.EApplication_PresentationNewSlideEventHandler(
-    Application_PresentationNewSlide);
+            client = new DiscordRpcClient(ClientID, true, DiscordPipe);
+            client.Logger = new DiscordRPC.Logging.ConsoleLogger() { Level = DiscordLogLevel, Coloured = true };
+
+            client.Initialize();
+
+            client.SetPresence(presence);
+
+            Debug.Print("aaa");
+            this.Application.PresentationNewSlide += 
+                new PowerPoint.EApplication_PresentationNewSlideEventHandler(
+                Application_PresentationNewSlide);
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
-
+            client.Dispose();
         }
 
-        void Application_PresentationNewSlide(PowerPoint.Slide Sld)
+        private void Application_PresentationNewSlide(PowerPoint.Slide Sld)
         {
             PowerPoint.Shape textBox = Sld.Shapes.AddTextbox(
                 Office.MsoTextOrientation.msoTextOrientationHorizontal, 0, 0, 500, 50);
