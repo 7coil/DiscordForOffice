09a171c0b751d9ca90ee42884661ed50a433e3b3
Leondro Lio <admin@moustacheminer.com>
2018-07-26 19:27:06 +0100

    Initial files

---
+++ DiscordForPowerPoint/ThisAddIn.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Office = Microsoft.Office.Core;
+
+namespace DiscordForPowerPoint
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            this.Application.PresentationNewSlide +=
+    new PowerPoint.EApplication_PresentationNewSlideEventHandler(
+    Application_PresentationNewSlide);
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+
+        }
+
+        void Application_PresentationNewSlide(PowerPoint.Slide Sld)
+        {
+            PowerPoint.Shape textBox = Sld.Shapes.AddTextbox(
+                Office.MsoTextOrientation.msoTextOrientationHorizontal, 0, 0, 500, 50);
+            textBox.TextFrame.TextRange.InsertAfter("This text was added by using code.");
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
